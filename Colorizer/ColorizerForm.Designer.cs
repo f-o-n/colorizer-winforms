5e3c3bbb2c1a4237c6a51ec153c4d1f82040114f
Franz Gasser <f.gasser@tricentis.com>
2020-05-18 23:14:17 +0800

    Initial commit

---
+++ Colorizer/ColorizerForm.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Colorizer
+{
+    partial class ColorizerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel = new System.Windows.Forms.Panel();
+            this.linkLabel = new System.Windows.Forms.LinkLabel();
+            this.previewPicture = new System.Windows.Forms.PictureBox();
+            this.sourceTextBox = new System.Windows.Forms.TextBox();
+            this.destinationTextBox = new System.Windows.Forms.TextBox();
+            this.destinationButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.sourceButton = new System.Windows.Forms.Button();
+            this.apiKeyTextBox = new System.Windows.Forms.TextBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.runButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.previewPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel.Controls.Add(this.linkLabel);
+            this.panel.Controls.Add(this.previewPicture);
+            this.panel.Controls.Add(this.sourceTextBox);
+            this.panel.Controls.Add(this.destinationTextBox);
+            this.panel.Controls.Add(this.destinationButton);
+            this.panel.Controls.Add(this.progressBar);
+            this.panel.Controls.Add(this.sourceButton);
+            this.panel.Controls.Add(this.apiKeyTextBox);
+            this.panel.Controls.Add(this.statusLabel);
+            this.panel.Controls.Add(this.runButton);
+            this.panel.Location = new System.Drawing.Point(0, 0);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(751, 473);
+            this.panel.TabIndex = 10;
+            // 
+            // linkLabel
+            // 
+            this.linkLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkLabel.AutoSize = true;
+            this.linkLabel.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel.Location = new System.Drawing.Point(642, 444);
+            this.linkLabel.Name = "linkLabel";
+            this.linkLabel.Size = new System.Drawing.Size(13, 13);
+            this.linkLabel.TabIndex = 19;
+            this.linkLabel.TabStop = true;
+            this.linkLabel.Text = "?";
+            this.linkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel_LinkClicked);
+            // 
+            // previewPicture
+            // 
+            this.previewPicture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.previewPicture.Location = new System.Drawing.Point(11, 64);
+            this.previewPicture.Name = "previewPicture";
+            this.previewPicture.Size = new System.Drawing.Size(728, 353);
+            this.previewPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.previewPicture.TabIndex = 15;
+            this.previewPicture.TabStop = false;
+            // 
+            // sourceTextBox
+            // 
+            this.sourceTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sourceTextBox.Location = new System.Drawing.Point(11, 12);
+            this.sourceTextBox.Name = "sourceTextBox";
+            this.sourceTextBox.Size = new System.Drawing.Size(647, 20);
+            this.sourceTextBox.TabIndex = 10;
+            // 
+            // destinationTextBox
+            // 
+            this.destinationTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.destinationTextBox.Location = new System.Drawing.Point(11, 38);
+            this.destinationTextBox.Name = "destinationTextBox";
+            this.destinationTextBox.Size = new System.Drawing.Size(647, 20);
+            this.destinationTextBox.TabIndex = 11;
+            // 
+            // destinationButton
+            // 
+            this.destinationButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.destinationButton.Location = new System.Drawing.Point(664, 38);
+            this.destinationButton.Name = "destinationButton";
+            this.destinationButton.Size = new System.Drawing.Size(75, 20);
+            this.destinationButton.TabIndex = 12;
+            this.destinationButton.Text = "Destination";
+            this.destinationButton.UseVisualStyleBackColor = true;
+            this.destinationButton.Click += new System.EventHandler(this.destinationButton_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(221, 441);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(437, 20);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 14;
+            // 
+            // sourceButton
+            // 
+            this.sourceButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.sourceButton.Location = new System.Drawing.Point(664, 12);
+            this.sourceButton.Name = "sourceButton";
+            this.sourceButton.Size = new System.Drawing.Size(75, 20);
+            this.sourceButton.TabIndex = 13;
+            this.sourceButton.Text = "Source";
+            this.sourceButton.UseVisualStyleBackColor = true;
+            this.sourceButton.Click += new System.EventHandler(this.sourceButton_Click);
+            // 
+            // apiKeyTextBox
+            // 
+            this.apiKeyTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.apiKeyTextBox.Location = new System.Drawing.Point(11, 441);
+            this.apiKeyTextBox.Name = "apiKeyTextBox";
+            this.apiKeyTextBox.Size = new System.Drawing.Size(204, 20);
+            this.apiKeyTextBox.TabIndex = 18;
+            this.apiKeyTextBox.Text = "quickstart-QUdJIGlzIGNvbWluZy4uLi4K";
+            this.apiKeyTextBox.Leave += new System.EventHandler(this.apiKeyTextBox_Leave);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.statusLabel.AutoEllipsis = true;
+            this.statusLabel.Location = new System.Drawing.Point(11, 422);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(675, 16);
+            this.statusLabel.TabIndex = 17;
+            this.statusLabel.DoubleClick += new System.EventHandler(this.statusLabel_DoubleClick);
+            // 
+            // runButton
+            // 
+            this.runButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.runButton.Location = new System.Drawing.Point(664, 441);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(75, 20);
+            this.runButton.TabIndex = 16;
+            this.runButton.Text = "Run";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // ColorizerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 473);
+            this.Controls.Add(this.panel);
+            this.MinimumSize = new System.Drawing.Size(768, 512);
+            this.Name = "ColorizerForm";
+            this.ShowIcon = false;
+            this.Text = "Colorizer by cyberfon";
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.previewPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.PictureBox previewPicture;
+        private System.Windows.Forms.TextBox sourceTextBox;
+        private System.Windows.Forms.TextBox destinationTextBox;
+        private System.Windows.Forms.Button destinationButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button sourceButton;
+        private System.Windows.Forms.TextBox apiKeyTextBox;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.LinkLabel linkLabel;
+        private System.Windows.Forms.ToolTip toolTip;
+    }
+}
---
+++ Colorizer/ColorizerForm.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+using System.Runtime.Serialization.Json;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace Colorizer
+{
+    public partial class ColorizerForm : Form
+    {
+
+        public ColorizerForm()
+        {
+            InitializeComponent();
+            SetupControls();
+        }
+
+        protected void SetupControls()
+        {
+            sourceTextBox.Text = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            destinationTextBox.Text = sourceTextBox.Text + "\\Colorized";
+            apiKeyTextBox.Text = ApiKey;
+
+            toolTip.SetToolTip(statusLabel, "Double-click to copy message to clipboard");
+            toolTip.SetToolTip(linkLabel, "Image Colorization API on DeepAi by Jason Antic");
+            toolTip.SetToolTip(apiKeyTextBox, "DeepAI API Key - Visit https://deepai.org/ to create one");
+        }
+
+        public string ApiKey
+        {
+            get {
+
+                var apiKey = Properties.Settings.Default["ApiKey"].ToString();
+                if (string.IsNullOrEmpty(apiKey))
+                {
+                    apiKey = "quickstart-QUdJIGlzIGNvbWluZy4uLi4K";
+                    ApiKey = apiKey;
+                }
+
+                return apiKey;
+            }
+            set {
+
+                Properties.Settings.Default["ApiKey"] = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        public void ProcessFolder(string inputFolder, string outputFolder)
+        {
+            var apiKey = ApiKey;
+            var inputDir = new DirectoryInfo(inputFolder);
+            var files = inputDir.GetFiles()
+                .Where(i => MimeMapping.GetMimeMapping(i.FullName).StartsWith("image/")).ToList();
+            
+            if (!Directory.Exists(outputFolder)) Directory.CreateDirectory(outputFolder);
+
+            if (files.Count == 0)
+                SetStatusMessage("Source folder does not contain any images");
+
+            SetProgressBar(0, 0, files.Count);
+
+            foreach (var file in files)
+            {
+                //Get unique file path for new file
+                var outputFile = "";
+                var count = 0;
+                do
+                {
+                    var nameExtension = count++ == 0 ? "_colorized" : "_colorized" + count;
+                    var fileName = Path.GetFileNameWithoutExtension(Path.Combine(outputFolder, file.Name));
+                    var outputFileName = fileName + nameExtension + file.Extension;
+                    outputFile = Path.Combine(outputFolder, outputFileName);
+
+                } while (File.Exists(outputFile));
+
+                try
+                {
+                    ColorizeImage(file.FullName, outputFile, apiKey);
+                }
+                catch (Exception e)
+                {
+                    SetStatusMessage(e.Message);
+                    return;
+                }
+
+            }
+        }
+
+        public void ColorizeImage(string inputImagePath, string outputImagePath, string apiKey)
+        {
+            /*DeepAi Library is like 180mb...
+            var api = new DeepAI_API(apiKey: apiKey);
+            var resp = api.callStandardApi("colorizer", new
+            {
+                image = File.OpenRead(inputImagePath),
+            });*/
+            
+            using (WebClient client = new WebClient())
+            {
+
+                string responseString = "";
+                client.Headers.Add("api-key", apiKey);
+
+                try
+                {
+                    var base64Image = Convert.ToBase64String(File.ReadAllBytes(inputImagePath));
+                    var nvCollection = new NameValueCollection() {{ "image", base64Image }};
+                    var response = client.UploadValues("https://api.deepai.org/api/colorizer", nvCollection);
+                    responseString = Encoding.ASCII.GetString(response);
+                }
+                catch (WebException e)
+                {
+                    if (e.Response == null)
+                    {
+                        SetStatusMessage($@"Upload Error: '{e.Message}'");
+                        return;
+                    }
+
+                    using (var r = new StreamReader(e.Response.GetResponseStream() ?? throw new InvalidOperationException()))
+                    {
+                        var errorResponse = r.ReadToEnd();
+                        var status = GetValueFromJson(errorResponse, "//status");
+                        SetStatusMessage(!string.IsNullOrEmpty(status) ? status : $@"Upload error '{errorResponse}'");
+                    }
+
+                    return;
+                }
+
+                var outputUrl = GetValueFromJson(responseString, "//output_url");
+
+                if (string.IsNullOrEmpty(outputUrl))
+                {
+                    SetStatusMessage($@"Response error '{responseString}'");
+                    return;
+                }
+
+                SetStatusMessage($@"Uploaded image '{inputImagePath}'");
+
+                client.DownloadFileCompleted += DownloadCompletedHandler;
+                client.DownloadFileAsync(new Uri(outputUrl), outputImagePath, outputImagePath);
+                
+            }
+        }
+
+        protected void DownloadCompletedHandler(object sender, AsyncCompletedEventArgs e)
+        {
+            var file = e.UserState.ToString();
+            var fileName = Path.GetFileName(file);
+
+            try
+            {
+                SetPreviewImage(file);
+                SetStatusMessage($@"Downloaded image '{fileName}'");
+            }
+            catch (Exception)
+            {
+                SetStatusMessage($@"Could not preview downloaded image '{fileName}'");
+            }
+            
+            SetProgressBar(progressBar.Value+1);
+
+            if (progressBar.Value == progressBar.Maximum)
+            {
+                SetStatusMessage("Done!");
+            }
+        }
+
+        public void SetProgressBar(int value, int? min = null, int? max = null)
+        {
+            progressBar.Invoke(new Action(() =>
+            {
+                if (min != null) progressBar.Minimum = min.Value;
+                if (max != null) progressBar.Maximum = max.Value;
+                progressBar.Value = value;
+            }));
+        }
+
+        public void SetEditMode(bool enabled)
+        {
+            panel.Invoke(new Action(() =>
+            {
+                panel.Enabled = enabled;
+            }));
+        }
+
+        public void SetStatusMessage(string message)
+        {
+            statusLabel.Invoke(new Action(() =>
+            {
+                statusLabel.Text = message;
+            }));
+        }
+
+        public void SetPreviewImage(string imagePath)
+        {
+            Image image;
+            using (var bmpTemp = new Bitmap(imagePath))
+            {
+                image = new Bitmap(bmpTemp);
+            }
+
+            previewPicture.Invoke(new Action(() =>
+            {
+                previewPicture.Image?.Dispose();
+                previewPicture.Image = image;
+            }));
+        }
+
+        protected void sourceButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                sourceTextBox.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        protected void destinationButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                destinationTextBox.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        protected void runButton_Click(object sender, EventArgs e)
+        {
+            var thread = Task.Factory.StartNew(() =>
+            {
+                SetEditMode(false);
+                SetStatusMessage("");
+                ProcessFolder(sourceTextBox.Text, destinationTextBox.Text);
+                SetEditMode(true);
+            });
+        }
+
+        protected void linkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://deepai.org/machine-learning-model/colorizer");
+        }
+
+        protected void apiKeyTextBox_Leave(object sender, EventArgs e)
+        {
+            ApiKey = apiKeyTextBox.Text;
+        }
+
+        protected void statusLabel_DoubleClick(object sender, EventArgs e)
+        {
+            Clipboard.SetText(statusLabel.Text);
+        }
+
+        public static string GetValueFromJson(string json, string path)
+        {
+            if (string.IsNullOrEmpty(json)) return null;
+            var jsonReader = JsonReaderWriterFactory.CreateJsonReader(
+                Encoding.UTF8.GetBytes(json),
+                new System.Xml.XmlDictionaryReaderQuotas());
+            var root = XElement.Load(jsonReader);
+            return root.XPathSelectElement(path)?.Value;
+        }
+    }
+}
